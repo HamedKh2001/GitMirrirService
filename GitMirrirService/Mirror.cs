d109cc7b7c4a21d6b2a8d4cc599dd2a97032f4c1
Hamed <foodyoy@gmail.com>
2023-02-18 23:04:14 +0330

    init

---
+++ GitMirrirService/Mirror.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+namespace GitMirrirService
+{
+    public class Mirror
+    {
+        static string SourceUrl = string.Empty;
+        static string DestinationUrl = string.Empty;
+        static string WorkingDirectory = Environment.CurrentDirectory;
+        public static void StarToMirror()
+        {
+            Console.WriteLine($"Enter Source Git Url?");
+            SourceUrl = InputValidator(Console.ReadLine());
+
+            CommandExecutor($"git clone --mirror {SourceUrl}", WorkingDirectory);
+            Console.WriteLine($"Enter Destination Git Url?");
+            DestinationUrl = InputValidator(Console.ReadLine());
+            WorkingDirectory = Path.Combine(WorkingDirectory, SourceUrl.Split('/').ToList().Last());
+            CommandExecutor($"git remote set-url --push origin {DestinationUrl}", WorkingDirectory);
+            Task.Delay(10000).Wait();
+            CommandExecutor($"git push --mirror", WorkingDirectory);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Mirroring is Done :)");
+            Console.ReadLine();
+        }
+
+
+        static string InputValidator(string input)
+        {
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Invalid Input...Try Again...");
+                Console.ForegroundColor = ConsoleColor.White;
+                InputValidator(Console.ReadLine());
+            }
+            return string.Empty;
+        }
+
+        static void CommandExecutor(string command, string workingDirectory)
+        {
+            command = "/c " + command;
+            var p = new Process
+            {
+                StartInfo =
+                    {
+                        FileName = "C:\\Windows\\system32\\cmd.exe",
+                        WorkingDirectory = workingDirectory,
+                        Arguments = command
+                    }
+            };
+            p.Start();
+        }
+    }
+}
---
+++ GitMirrirService/MirrorService.cs
@@ -0,0 +1,52 @@
+﻿namespace GitMirrirService
+{
+    public class MirrorService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private readonly ILogger<MirrorService> _logger;
+        private Timer? _timer = null;
+        private TimeSpan timeout;
+
+        public MirrorService(ILogger<MirrorService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+
+            var hours = Convert.ToInt32(configuration.GetSection("PeriodTime")["Hour"]);
+            var minutes = Convert.ToInt32(configuration.GetSection("PeriodTime")["Minutes"]);
+            var seconds = Convert.ToInt32(configuration.GetSection("PeriodTime")["Seconds"]);
+            timeout = new TimeSpan(hours, minutes, seconds);
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Hosted Service Started To Mirroring.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, timeout);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        private void DoWork(object? state)
+        {
+            Mirror.StarToMirror();
+            var count = Interlocked.Increment(ref executionCount);
+
+            _logger.LogInformation(
+                "Count Of Mirrors: {Count}", count);
+        }
+    }
+}
