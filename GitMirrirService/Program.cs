aa514f4ff55da98b21b44c196542bf28f76f5fc0
Hamed <foodyoy@gmail.com>
2023-05-07 15:04:52 +0330

    remove swagger nuget

--- GitMirrirService/Program.cs
+++ GitMirrirService/Program.cs
@@ -28,18 +28,6 @@ builder.Services.AddHostedService<QuartzHostedService>();
 
 builder.Host.UseWindowsService();
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 app.Run();
